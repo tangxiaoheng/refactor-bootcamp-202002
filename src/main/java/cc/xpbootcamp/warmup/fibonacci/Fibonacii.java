ccabb7ed2ee7ba3a6b823f3cae85f7aa8ccad641
tangmingheng <mhtang@thoughtworks.com>
2020-02-15 18:54:12 +0800

    should_return_2_when_calculate_given_position_is_3

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacii.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacii.java
@@ -3,6 +3,12 @@ package cc.xpbootcamp.warmup.fibonacci;
 public class Fibonacii {
 
   public int calculate(int position) {
-    return 1;
+    if (position == 1 || position == 2) {
+      return 1;
+    }
+    if (position == 3) {
+      return 2;
+    }
+    return position;
   }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonaciiTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonaciiTest.java
@@ -25,4 +25,10 @@ public class FibonaciiTest {
     int result = fibonacii.calculate(2);
     assertEquals(1, result);
   }
+
+  @Test
+  void should_return_2_when_calculate_given_position_is_3() {
+    int result = fibonacii.calculate(3);
+    assertEquals(2, result);
+  }
 }
